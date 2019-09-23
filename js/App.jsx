3c6e599afba8945f98fbc13f7f5594e21ab99f86
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2019-09-23 14:53:35 +0200

    Postepy 23.09.19r godz 14:53

---
+++ js/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class App extends React.Component {
+
+
+
+    render() {
+        return (
+            <div className="app">
+                <h1>Dziala kalkulator 2.0</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
